7df61f5414fd93b68aab2621cdbcb068483ea548
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2024-05-21 09:33:28 +0200

    First attempt to package trac python package

---
+++ pkgs/by-name/tr/trac/package.nix
@@ -0,0 +1,37 @@
+{ fetchPypi
+, python3
+, python3Packages
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "Trac";
+  version = "1.6";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-Ydc8YfZw1o/8NGgp0ksvHSBQqlYapxy5ji+0OZLCcwQ=";
+  };
+
+  nativeBuildInputs = [];
+  buildInputs = [];
+  propagatedBuildInputs = [
+    python3Packages.setuptools
+    python3Packages.jinja2 
+    python3Packages.pygments
+    python3Packages.psycopg2
+  ];
+  pythonImportsCheck = [ "trac" ];
+
+
+  outputs = [ "out" ];
+  
+  checkPhase = ''
+    runHook preCheck
+    ${python3.interpreter} -m unittest \
+      -k tests \
+      -k admin \
+      -k ticket \
+      # Ignore wiki folder, broken (won't even start, test.py is badly written?)
+      # -k wiki
+    runHook postCheck
+  '';
+}
